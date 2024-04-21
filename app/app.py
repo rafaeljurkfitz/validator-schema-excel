22550e11b0a663dcf75f19a440f9198a2def3ff5
Rafael Jürkfitz <rafaeljurkfitz@hotmail.com>
2024-04-21 20:03:51 -0300

    My first test

---
+++ app/app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+# Title of app
+st.title("We are online!")
---
+++ tests/test_app.py
@@ -0,0 +1,28 @@
+""" test app """
+
+import subprocess
+from time import sleep
+
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver():
+    # Start the Streamlit in background
+    process = subprocess.Popen(["streamlit", "run", "app/app.py"])
+
+    # We need define out driver
+    driver = webdriver.Edge()
+    driver.set_page_load_timeout(5)
+    yield driver
+
+    # Close the WebDriver and the Streamlit after the tests
+    driver.quit()
+    process.kill()
+
+
+def test_app_opens(driver):
+    # Verify if the page opens
+    driver.get("http://localhost:8501")
+    sleep(5)
