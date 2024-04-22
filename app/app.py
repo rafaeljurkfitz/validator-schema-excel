3c131d8f61208261f5074044cd169c1ba93e219a
Rafael Jürkfitz <rafaeljurkfitz@hotmail.com>
2024-04-21 21:12:53 -0300

    add more tests and features did with TDD

--- app/app.py
+++ app/app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-# Title of app
-st.title("We are online!")
+st.set_page_config(page_title="Validador de schema excel")
+
+st.title("Insira o seu excel para validação.")
+
+file = st.file_uploader("Carregue seu arquivo Excel aqui", type=["xlsx"])
+
+if file is not None:
+    st.success("O schema do arquivo Excel está correto!")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,24 @@
 """ test app """
 
+import os
 import subprocess
 from time import sleep
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
 
 @pytest.fixture
 def driver():
     # Start the Streamlit in background
     process = subprocess.Popen(["streamlit", "run", "app/app.py"])
-
+    options = Options()
+    # Execute in mode headless
+    options.headless = True
     # We need define out driver
-    driver = webdriver.Firefox()
+    driver = webdriver.Firefox(options=options)
     driver.set_page_load_timeout(5)
     yield driver
 
@@ -25,4 +30,51 @@ def driver():
 def test_app_opens(driver):
     # Verify if the page opens
     driver.get("http://localhost:8501")
-    sleep(5)
+    sleep(2)
+
+
+def test_check_title_is(driver):
+    # Verify if the page opens
+    driver.get("http://localhost:8501")
+    # Verify if the page's title is
+    sleep(2)
+    # Capture the page's title
+    page_title = driver.title
+
+    # Verify if the page's title is the expected
+    expected_title = (
+        "Validador de schema excel"  # Replace with the actual expected title
+    )
+    assert page_title == expected_title
+
+
+def test_check_streamlit_h1(driver):
+    # Verify if the page opens
+    driver.get("http://localhost:8501")
+    # Verify if the page's title is
+    sleep(2)
+    # Capture the h1 page's title (<h1></h1>)
+    h1_element = driver.find_element(By.TAG_NAME, "h1")
+
+    # Verify if the h1 page's title is the expected
+    expected_element = (
+        "Insira o seu excel para validação."  # Replace with the actual expected title
+    )
+    assert h1_element.text == expected_element
+
+
+def test_check_user_can_insert_a_excel_and_receive_a_message(driver):
+    # Verify if the page opens
+    driver.get("http://localhost:8501")
+
+    sleep(3)
+
+    # Upload the file successfully
+    success_file_path = os.path.abspath("data/file.xlsx")
+    driver.find_element(By.CSS_SELECTOR, 'input[type="file"]').send_keys(
+        success_file_path
+    )
+
+    # Aguardar a mensagem de sucesso
+    sleep(3)
+    assert "O schema do arquivo Excel está correto!" in driver.page_source
