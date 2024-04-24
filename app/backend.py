ebe70e41b396626e55a86926b13f15de89985de3
Rafael Jürkfitz <rafaeljurkfitz@hotmail.com>
2024-04-24 15:49:17 -0300

    feat: add feature sql integration db

--- app/backend.py
+++ app/backend.py
@@ -1,5 +1,23 @@
+import os
+
 import pandas as pd
 from contract import Sales
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+# Lê as variáveis de ambiente
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+# Cria a URL de conexão com o banco de dados
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+# Carrega as variáveis de ambiente
+load_dotenv()
 
 
 def process_excel(uploaded_file):
@@ -19,9 +37,14 @@ def process_excel(uploaded_file):
                 raise ValueError(f"Erro na linha {index + 2}: {e}")
 
         # Retorna tanto o resultado da validação, os erros, quanto o DataFrame
-        return True, None
+        return df, True, None
 
     except ValueError as ve:
-        return False, str(ve)
+        return df, False, str(ve)
     except Exception as e:
-        return False, f"Erro inesperado: {str(e)}"
+        return df, False, f"Erro inesperado: {str(e)}"
+
+
+def save_dataframe_to_sql(df):
+    # Salva o DataFrame no banco de dados
+    df.to_sql("sales", con=DATABASE_URL, if_exists="replace", index=False)
